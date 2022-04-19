b723edec4cf23184f4134d49f5a98dd7ab2f3546
Denis Junior <junior.denis2354@outlook.com>
2022-04-19 16:18:31 -0300

    personalized bg/spacing;size menuItem

--- src/components/navigation/MenuItem.tsx
+++ src/components/navigation/MenuItem.tsx
@@ -2,11 +2,32 @@ import React from "react";
 
 import Link from "../actions/Link";
 import Text from "../presentation/Text";
+import { sizeProps } from "../types/size";
+import { spacingProps } from "../types/spacing";
 
-const MenuItem = ({ href, label }: { href: string; label: string }) => {
+const MenuItem = ({
+  href,
+  label,
+  withoutBg,
+  padding,
+  width,
+  color,
+}: {
+  href: string;
+  label: string;
+  withoutBg?: boolean;
+  color?: string;
+} & spacingProps &
+  sizeProps) => {
   return (
-    <Link href={href} bgColor="#F1F1F1" hoverColor="#fff" width="6rem" padding="10px">
-      <Text element="span" color="#000">
+    <Link
+      href={href}
+      bgColor={withoutBg ? "rgba(0,0,0,0)" : "#F1F1F1"}
+      hoverColor={withoutBg ? "rgba(0,0,0,0)" : "#fff"}
+      width={width || "6rem"}
+      padding={padding || "10px"}
+    >
+      <Text element="span" color={color || "#000"}>
         {label}
       </Text>
     </Link>
